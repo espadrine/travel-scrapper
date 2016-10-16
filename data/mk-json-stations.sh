1f25f1051757b8a93943e618685031ab8f063c7c
Thaddee Tyl <thaddee.tyl@gmail.com>
2016-10-16 20:02:31 +0200

    Add geolocation to stations

--- data/mk-json-stations.sh
+++ data/mk-json-stations.sh
@@ -2,9 +2,9 @@
 
 tmpfile=`mktemp`
 cat data/stations.csv | tail -n +2 |  # The first line is a header.
-  cut -f 1,2,10,12 -d ';' |  # Keep the id and the name. 
+  cut -f 1,2,6,7,10,12 -d ';' |  # id, name, lat, long, country, timezone
   sed 's/"/\\"/g' |    # JSON string escaping.
-  sed 's/^\([0-9]\+\);\([^;]*\);\([^;]*\);\([^;]*\)$/{"id":"\1","name":"\2","country":"\3","timezone":"\4"},/' |
+  sed 's/^\([0-9]\+\);\([^;]*\);\([^;]*\);\([^;]*\);\([^;]*\);\([^;]*\)$/{"id":"\1","name":"\2","lat":"\3","long":"\4","country":"\5","timezone":"\6"},/' |
   head -c -2 > $tmpfile  # Remove the last comma in the list.
 (echo -n '['; cat $tmpfile; echo ']') > data/stations.json
 rm $tmpfile
